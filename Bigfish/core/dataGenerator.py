d6f8e1622f176b328883076e893db7a81be7e951
BurdenBear <541795600@qq.com>
2015-11-25 21:57:39 +0800

    1

--- Bigfish/core/dataGenerator.py
+++ Bigfish/core/dataGenerator.py
@@ -6,9 +6,24 @@ Created on Wed Nov 25 20:41:04 2015
 """
 
 class dataGenerator:
-    """
-    数据生成器
+    """fetch data from somewhere and put dataEvent into eventEngine
+        数据生成器
+    Attrs:
+        __engine:the eventEngine where dataEvent putted into        
+        __symols:symbol of assets
+        __start_time:start time of data
+        __end_time:end tiem of data
+        __time_frame_bits:bits contain information of which time frame to use
     """
     
-    def __init__:
+    def __init__(self, engine, symbols, start_time, end_time, time_frame_bits):
+        self.__engine = engine
+        self.__symbols = symbols
+        self.__start_time = start_time
+        self.__end_time = end_time
+        self.__time_frame_bits = time_frame_bits
+    
+    def __start():
+        
+    
     
